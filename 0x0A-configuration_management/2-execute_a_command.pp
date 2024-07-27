b6cc82a82d8bd9c8b836101668b6d118a5de5153
clementmuchai <125451515+clementmuchai@users.noreply.github.com>
2024-07-27 16:37:24 +0300

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-# This manifest uses the exec resource to kill a process named 'killmenow'
-exec { 'kill_process':
-  command => '/usr/bin/pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => 'pgrep -f killmenow',
+# puppet manifest to execute a command
+
+exec {'killmenow':
+    command  => 'pkill -TERM -f killmenow',
+    provider => shell,
+    returns  => 0,
 }
