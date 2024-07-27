43d1ff0fa0bb2086bfb6ea9b7f0628283811f7be
clementmuchai <125451515+clementmuchai@users.noreply.github.com>
2024-07-27 16:42:32 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#  manifest installs Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+# This manifest installs Flask version 2.1.0 using the pip3 provider
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
