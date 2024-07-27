e59c97ee9ef01242bce01bf25e6ebbd62dadf656
Charleskahuho <125451515+charleskahuho@users.noreply.github.com>
2024-07-27 16:30:31 +0300

    files added

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# The manifest to kill  a process named 'killmenow'
+exec { 'kill_process':
+  command => '/usr/bin/pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => 'pgrep -f killmenow',
+}
